4d5b63ec1662b70c3f169451f8be7da0e790813d
Siddharth Govardhane <119425832+Siddharth1047@users.noreply.github.com>
2023-04-19 13:05:12 +0530

    Add files via upload

---
+++ Day-7.py
@@ -0,0 +1,17 @@
+# Find duplicate in an array
+
+def duplicate(arr,n):
+    seen = {}
+    duplicates = set()
+
+    for num in arr:
+        if num in seen:
+            seen[num] += 1
+            duplicates.add(num)
+        else:
+            seen[num] = 1
+        
+    return duplicates
+
+result = duplicate([1,2,3,5,3,2,7,6,9,1,2,3],12)
+print(result)
