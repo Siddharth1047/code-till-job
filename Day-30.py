1e666334f350a79f1fc14ab3e327dc6c352d5204
Siddharth Govardhane <119425832+Siddharth1047@users.noreply.github.com>
2023-05-26 20:58:35 +0530

    N-Queen

--- Day-30.py
+++ Day-30.py
@@ -1,21 +1,58 @@
-def print_a():
-    # Print the top half of the "A"
-    for i in range(5):
-        for j in range(9):
-            if j == 4 - i or j == 4 + i:
-                print("*", end="")
-            else:
-                print(" ", end="")
+# Implement Solution to N-Queen Problem Using BackTracking 
+
+global N
+N = 4
+
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            print(board[i][j], end=' ')
         print()
 
-    # Print the bottom half of the "A"
-    for i in range(3):
-        for j in range(9):
-            if j == 1 or j == 7:
-                print("*", end="")
-            else:
-                print(" ", end="")
-        print()
+def isSafe(board, row, col):
 
-# Call the function to print the "A"
-print_a()
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+        
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+        
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+        
+    return True
+
+def solveUtil(board, col):
+
+    if col >= N:
+        return True
+    
+    for i in range(N):
+
+        if isSafe(board, i, col):
+            board[i][col] = 1
+
+            if solveUtil(board, col+1) == True:
+                return True
+            
+            board[i][col] = 0
+
+    return False
+
+def solveNQ():
+    board = [[0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0]]
+    
+    if solveUtil(board, 0) == False:
+        print("NO SOLUTION")
+        return False
+    
+    printSolution(board)
+    return True
+
+solveNQ()
